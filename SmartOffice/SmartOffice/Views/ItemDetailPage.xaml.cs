b2d32c6dacb24cb48e495da363351e5100d7c87e
F1ower <Daniil.shirkin005@gmail.com>
2023-10-04 19:03:53 +0300

    InitialCommit

---
+++ SmartOffice/SmartOffice/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SmartOffice.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SmartOffice.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
